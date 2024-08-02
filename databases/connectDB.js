063c4029eb667c77a4112e5155b3d11656aa4d85
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2024-08-02 16:36:41 +0530

    made changes

--- databases/connectDB.js
+++ databases/connectDB.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("Attempting to connect to MongoDB...");
     await mongoose.connect(process.env.MONGODB_URL, {
-      serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+      serverSelectionTimeoutMS: 1000, // Increase timeout to 30 seconds
     });
-    console.log("MongoDB connected");
+    console.log("DB is connected");
   } catch (err) {
-    console.error("MongoDB connection error:", err.message);
+    console.error("DB connection error:", err.message);
     process.exit(1);
   }
 };
